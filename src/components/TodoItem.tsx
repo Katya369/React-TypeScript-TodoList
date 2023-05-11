1b9ec3d4684e09d7f6277bf10618c5339f7639ed
nektarina369 <nekrev@gmail.com>
2023-05-11 15:02:43 +0300

    Created Project with TypeScript, ReactContext and Hooks

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,5 @@
+import classes from './TodoItem.module.css';
+const TodoItem: React.FC<{text: string; onRemoveTodo: (event: React.MouseEvent) => void}> = (props) => {
+return <li onClick={props.onRemoveTodo} className={classes.item}>{props.text}</li>
+};
+export default TodoItem;
